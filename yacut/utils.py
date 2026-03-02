301132feff9390d1a4bf9cea97d3a13c7da8cf9c
unknown <dima.mr-smith@yandex.ru>
2026-03-02 18:47:41 +0300

    fix tests v4

--- yacut/utils.py
+++ yacut/utils.py
@@ -3,13 +3,11 @@ import string
 from .models import URLMap
 
 CHARSET = string.ascii_letters + string.digits
-MIN_LENGTH = 6
-MAX_LENGTH = 10
+SHORT_ID_LENGTH = 6
 
 
 def get_unique_short_id():
     while True:
-        length = random.randint(MIN_LENGTH, MAX_LENGTH)
-        short_id = ''.join(random.choices(CHARSET, k=length))
+        short_id = ''.join(random.choices(CHARSET, k=SHORT_ID_LENGTH))
         if not URLMap.query.filter_by(short=short_id).first():
             return short_id
